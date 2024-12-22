daedd28a2aa5cfe0583235832321c56ccb0a6627
TomF777 <87227809+TomF777@users.noreply.github.com>
2024-12-22 13:15:35 +0100

    Update caesar.c

--- week_2_Arrays_in_C/caesar.c
+++ week_2_Arrays_in_C/caesar.c
@@ -1,3 +1,22 @@
+/*
+Caesar used to “encrypt” (i.e., conceal in a reversible way) confidential messages by shifting each letter therein by some number of places. 
+For instance, he might write A as B, B as C, C as D, …, and, wrapping around alphabetically, Z as A. 
+And so, to say HELLO to someone, Caesar might write IFMMP instead. 
+Upon receiving such messages from Caesar, recipients would have to “decrypt” them by shifting letters in the opposite direction by the same number of places.
+
+Formally, Caesar’s algorithm (i.e., cipher) encrypts messages by “rotating” each letter by 'k' positions.
+More formally, if 'p' is some plaintext (i.e., an unencrypted message), 'pi' is the 'ith' character in 'p'
+and 'k' is a secret key (i.e., a non-negative integer), then each letter 'ci', in the ciphertext, is computed as:
+ci = (pi + k) % 26
+
+- Program must accept a single command-line argument, a non-negative integer.
+- If  program is executed without any command-line arguments or with more than one command-line argument, your program should print an error message.
+- Program must output "plaintext:  " and then prompt the user for a string of plaintext.
+- Program must output "ciphertext: " followed by the plaintext’s corresponding ciphertext, 
+  with each alphabetical character in the plaintext “rotated” by k positions; non-alphabetical characters should be outputted unchanged.
+- Program must preserve case: capitalized letters, though rotated, must remain capitalized letters; lowercase letters, though rotated, must remain lowercase letters.
+*/
+
 #include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
