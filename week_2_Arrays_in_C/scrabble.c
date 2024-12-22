e776ca576b2e098f5af58a145f1cbb9a5f6cfbfe
TomF777 <87227809+TomF777@users.noreply.github.com>
2024-12-22 13:33:17 +0100

    Update scrabble.c

--- week_2_Arrays_in_C/scrabble.c
+++ week_2_Arrays_in_C/scrabble.c
@@ -4,7 +4,7 @@ In the game of Scrabble, players create words to score points, and the number of
 A B C D E F G H I J K L M N O P Q  R S T U V W X Y Z
 1 3 3 2 1 4 2 4 1 8 5 1 3 1 1 3 10 1 1 1 1 4 4 8 4 10
 
-Th program should prompt for input twice: once for “Player 1” to input their word and once for “Player 2” 
+The program should prompt for input twice: once for “Player 1” to input their word and once for “Player 2” 
 to input their word. 
 Then, depending on which player scores the most points, 
 the program should either print “Player 1 wins!”, “Player 2 wins!”, or “Tie!” (in the event the two players score equal points).
